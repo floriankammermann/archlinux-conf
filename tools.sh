04e70f74c60d903407638dc63f2543a52eaa92aa
floriankammermann <florian@kammermann.me>
2016-06-07 22:00:41 +0200

    first commit of the archlinux config

---
+++ tools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+pacman -S xorg-server;
+pacman -S xorg-utils;
+pacman -S xf86-video-intel;
+pacman -S xterm;
+pacman -S base-devel;
+pacman -S chromium;
+pacman -S firefox;
+pacman -S fluxbox;
+pacman -s alsa-firmware;
+pacman -S alsa-lib;
+pacman -S alsa-oss;
+pacman -S alsa-tools;
+pacman -S alsa-utils;
+pacman -S wicd-gtk;
+pacman -S binutils;
+pacman -S gcc;
+pacman -S pkg-config;
+pacman -S make;
+pacman -S fakeroot;
+pacman -S ttf-dejavu;
+pacman -S ttf-liberation;
+pacman -S terminus-font;
+pacman -S adobe-source-code-pro-fonts;
