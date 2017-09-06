0ce74ab5430a7dc7f53e00682bfee3a157990e6b
Kammermann Florian, INI-DEV-FDN-ENB <Florian.Kammermann@swisscom.com>
2017-09-06 12:02:28 +0200

    added keepass client

---
+++ keepass/install_kpcli.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# download the perl command line client
+wget https://sourceforge.net/projects/kpcli/files/kpcli-3.1.pl/download -O kbpcli-3.1.pl
+
+# install the needed perl modules
+sudo cpan Clone
+sudo cpan Crypt
+sudo cpan Crypt::Rijndael
+sudo cpan Sort::Naturally
+sudo cpan Term::ReadKey
+sudo cpan Term::ShellUI
+sudo cpan File::KeePass
+sudo cpan Term::ReadLine::Gnu
+sudo cpan XML::Parser
+sudo cpan Capture::Tiny
+sudo cpan Clipboard
+
+# install xclip
+pacman -S xclip
